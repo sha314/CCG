6d93b0d56bfca4f2cdab41ac92c251fa28e44c37
StandSteel <shahnoor3pl@gmail.com>
2017-05-23 23:13:14 +0600

    new function is included
    parsing command line arguments for character-set is performed by functions

---
+++ src/ccg.h
@@ -0,0 +1,19 @@
+
+#ifndef CCG_CCG_H
+#define CCG_CCG_H
+
+#include <cstdlib>
+#include <ctime>
+
+extern bool verbose;
+
+void ccg(const char *char_set, size_t char_set_len,
+     const size_t WIDTH,  const char * filename);// NAF -> Not A File
+
+void help();
+
+double print_time_interval(clock_t t0, clock_t t1, bool flag);
+
+size_t count_combination(size_t char_set_len, size_t width);
+
+#endif
---
+++ src/parse_args.h
@@ -0,0 +1,9 @@
+
+#ifndef CCG_PARSE_ARGS_H
+#define CCG_PARSE_ARGS_H
+
+const char * substring(const char * chs, int &begin, char end);
+int characterSetLegth(int *arr, int len);
+const char * get_char_set(const char * chs);
+
+#endif
