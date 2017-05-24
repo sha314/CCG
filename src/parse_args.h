770c5a94507d90dda308a8c45c3fba1ea70f9555
StandSteel <shahnoor3pl@gmail.com>
2017-05-24 22:23:21 +0600

    Update parse_args.h

--- src/parse_args.h
+++ src/parse_args.h
@@ -2,8 +2,10 @@
 #ifndef CCG_PARSE_ARGS_H
 #define CCG_PARSE_ARGS_H
 
-const char * substring(const char * chs, int &begin, char end);
+// const char * substring(const char * chs, int &begin, char end);
 int characterSetLegth(int *arr, int len);
 const char * get_char_set(const char * chs);
+int str_to_int(const char * chs, int &begin, char end);
+int char_to_int(char c);
 
 #endif
